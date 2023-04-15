08a058caa99c961142867c3154be780e306add6a
elif c <86270311+elif-c@users.noreply.github.com>
2023-04-15 16:50:29 +0300

    Added function doc strings

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ from colorama import Fore
 
 
 def read_todos(filepath="todos.rtf"):
+    """ Read the todo list items file and return the contents as a list."""
     with open(filepath, "r") as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
 
 def write_todos(todos_arg, filepath="todos.rtf"):
+    """ Write the modified todos list in the program onto the file specified."""
     with open(filepath, "w", encoding="utf-8") as file_local:
         file_local.writelines(todos_arg)
         # no need to return anything
