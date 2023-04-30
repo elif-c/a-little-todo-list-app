ba52845cf90272c58af044f1062a969f53384c5e
elif c <86270311+elif-c@users.noreply.github.com>
2023-04-30 19:21:23 +0300

    Edited "add" button, error handling

--- gui.py
+++ gui.py
@@ -1,54 +1,59 @@
-# from cli import display_time
 import functions
 import PySimpleGUI as gui  # sg is the convention
+import time
 
-# date_label = gui.Text(functions.display_time())
+gui.theme("DarkPurple1")
+
+date_label = gui.Text("", key="date", font=("Futura", 13), text_color="hot pink")
 label = gui.Text("Type in a to-do item:")
-input_box = gui.InputText(tooltip="Enter list item", key="input_todo")  # tooltip is the hover info
-add_button = gui.Button("Add")
+input_box = gui.InputText(tooltip="Enter list item", size=(44, 1), key="input_todo")  # tooltip is the hover info
+add_button = gui.Button(key="Add", size=15, image_source="add.png", tooltip="Add a task")
 list_box = gui.Listbox(values=functions.read_todos(), key="selected_todo",
-                       enable_events=True, size=[45, 10])
-edit_button = gui.Button("Edit")
-complete_button = gui.Button("Complete")
-remove_button = gui.Button("Remove")
-exit_button = gui.Button("Exit")
+                       enable_events=True, size=[43, 10])
+edit_button = gui.Button("Edit", size=8)
+complete_button = gui.Button("Complete", size=8)
+remove_button = gui.Button("Remove", size=8)
+exit_button = gui.Button("Exit", size=8)
 # see_finished = gui.Button("See Finished Tasks", key="see_finished")
 
 buttons_column = [[edit_button],
                   [complete_button],
                   [remove_button],
                   [exit_button]]
-layout = [[label],
+layout = [[date_label],
+          [label],
           [input_box, add_button],
-          [list_box, gui.Column(buttons_column)]]
+          [list_box, gui.Column(buttons_column, element_justification='center')]]
 
-finished_label = gui.Text("Finished Tasks", text_color="purple4")
-finished_item = gui.Input(key="chosen_finished")
-remove_finished_button = gui.Button("Remove", key="rm_finished")
-clear_button = gui.Button("Clear")
-go_back_button = gui.Button("Go Back")
-finished_box = gui.Listbox(values=functions.read_todos("finished_todos.rtf"),
-                           key="finished_todo", enable_events=True,
-                           size=[45, 10])
+# finished_label = gui.Text("Finished Tasks", text_color="purple4")
+# finished_item = gui.Input(key="chosen_finished")
+# remove_finished_button = gui.Button("Remove", key="rm_finished")
+# clear_button = gui.Button("Clear")
+# go_back_button = gui.Button("Go Back")
+# finished_box = gui.Listbox(values=functions.read_todos("finished_todos.rtf"),
+#                            key="finished_todo", enable_events=True,
+#                            size=[45, 10])
 
-finished_layout = [[finished_label],
-                   [finished_item, remove_finished_button],
-                   [finished_box, clear_button],
-                   [go_back_button]]
+# finished_layout = [[finished_label],
+#                    [finished_item, remove_finished_button],
+#                    [finished_box, clear_button],
+#                    [go_back_button]]
 
 # layout argument expects a list, which will be a list of object instances
 # items put in the inner brackets will be placed in one row
 window = gui.Window("To-Do App",
                     layout=layout,
-                    font=("Futura", 14))
-finished_window = gui.Window("To-Do App",
-                             layout=finished_layout,
-                             font=("Futura", 14))
+                    font=("Futura", 14),
+                    element_padding=4,
+                    text_justification="center",
+                    resizable=False)
+# finished_window = gui.Window("To-Do App",
+#                              layout=finished_layout,
+#                              font=("Futura", 14))
 
 while True:
-    event_key, value = window.read()  # event returns pressed button's label
-    print(event_key)
-    print(value)
+    event_key, value = window.read(timeout=200)  # event returns pressed button's label
+    window["date"].update(value=time.strftime("%b %d, %Y %I:%M%p"))
     match event_key:
         case "Add":
             todos = functions.read_todos()
@@ -60,37 +65,44 @@ while True:
                 window.refresh()
                 window["selected_todo"].update(values=todos)  # several values
         case "Edit":
-            editing_item = value["selected_todo"][0]
-            new_edited_item = value["input_todo"] + "\n"
-            todos = functions.read_todos()
-            index = todos.index(editing_item)
-            todos[index] = new_edited_item
-            functions.write_todos(todos)
-            window["selected_todo"].update(values=todos)
+            try:
+                editing_item = value["selected_todo"][0]
+                new_edited_item = value["input_todo"] + "\n"
+                todos = functions.read_todos()
+                index = todos.index(editing_item)
+                todos[index] = new_edited_item
+                functions.write_todos(todos)
+                window["selected_todo"].update(values=todos)
+            except IndexError:
+                gui.popup("Please select an item.", font=("Futura", 14))
         case "Remove":
-            removing_item = value["selected_todo"][0]
-            todos = functions.read_todos()
-            index = todos.index(removing_item)
-            todos.pop(index)
-            window.refresh()
-            functions.write_todos(todos)
-            window["selected_todo"].update(values=todos)
-            window["input_todo"].update(value="")
-            # add remove and clear functions for the finished list
-            # also update listbox with finished list
+            try:
+                removing_item = value["selected_todo"][0]
+                todos = functions.read_todos()
+                index = todos.index(removing_item)
+                todos.pop(index)
+                window.refresh()
+                functions.write_todos(todos)
+                window["selected_todo"].update(values=todos)
+                window["input_todo"].update(value="")
+            except IndexError:
+                gui.popup("Please select an item.", font=("Futura", 14))
         case "Complete":
-            complete_item = value["selected_todo"][0]
-            todos = functions.read_todos()
-            index = todos.index(complete_item)
-            item = todos.pop(index)
-            window.refresh()
-            functions.write_todos(todos)
-            finished = functions.read_todos("finished_todos.rtf")
-            finished.append(item)
-            window.refresh()
-            functions.write_todos(finished, "finished_todos.rtf")
-            window["selected_todo"].update(values=todos)
-            window["input_todo"].update(value="")
+            try:
+                complete_item = value["selected_todo"][0]
+                todos = functions.read_todos()
+                index = todos.index(complete_item)
+                item = todos.pop(index)
+                window.refresh()
+                functions.write_todos(todos)
+                finished = functions.read_todos("finished_todos.rtf")
+                finished.append(item)
+                window.refresh()
+                functions.write_todos(finished, "finished_todos.rtf")
+                window["selected_todo"].update(values=todos)
+                window["input_todo"].update(value="")
+            except IndexError:
+                gui.popup("Please select an item.", font=("Futura", 14))
         case "Exit":
             break
         # case "see_finished":
